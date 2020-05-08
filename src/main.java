1a0e88f71ffce496376a40bc3d80faf65356d755
sergei Shapovalov <senishapovalov@gmail.com>
2020-05-08 13:30:01 +0300

    end

---
+++ src/main.java
@@ -0,0 +1,16 @@
+public class main {
+  public static void main(String[] args) {
+        float bill=100.00f;
+        int payroll=1100;
+        int bonus;
+        boolean action =true;
+        if (payroll>=1100) {
+             bonus = payroll / 100;}
+        else {
+              bonus = 0; }
+
+
+
+        System.out.println(bonus);
+    }
+}
