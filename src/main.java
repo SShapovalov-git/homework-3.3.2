7b17bea0a05db0d67c2f6ced9750e9c97f2bf33d
sergei Shapovalov <senishapovalov@gmail.com>
2020-05-10 13:31:49 +0300

    mending

--- src/main.java
+++ src/main.java
@@ -1,16 +1,16 @@
 public class main {
-  public static void main(String[] args) {
-        float bill=100.00f;
-        int payroll=1100;
-        int bonus;
-        boolean action =true;
-        if (payroll>=1100) {
-             bonus = payroll / 100;}
-        else {
-              bonus = 0; }
+    public static void main(String[] args) {
+        float bill = 100.10f;
+        int payroll = 1500;
+        float balance;
+        boolean action = true;
+        if (payroll >= 1000) {
+            balance = bill + payroll + (payroll / 100);
+        } else {
+            balance = bill + payroll;
+        }
 
 
-
-        System.out.println(bonus);
+        System.out.println(balance);
     }
 }
